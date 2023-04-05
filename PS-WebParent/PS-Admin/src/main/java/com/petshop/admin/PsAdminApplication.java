15e7ae521e0a62fd7044de074c0730e487e1d1f4
MrTee <vokhanhloc1994@gmail.com>
2023-04-05 12:44:36 +0700

    create project

---
+++ PS-WebParent/PS-Admin/src/main/java/com/petshop/admin/PsAdminApplication.java
@@ -0,0 +1,13 @@
+package com.petshop.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsAdminApplication.class, args);
+	}
+
+}
---
+++ PS-WebParent/PS-Client/src/main/java/com/petshop/client/PsClientApplication.java
@@ -0,0 +1,13 @@
+package com.petshop.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsClientApplication.class, args);
+	}
+
+}

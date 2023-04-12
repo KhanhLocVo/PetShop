80f251503b76707ab6ff322f910f96f396a6c39c
MrTee <vokhanhloc1994@gmail.com>
2023-04-12 12:35:38 +0700

    code create new user function, update user function, delete user function and update user enabled status

--- PS-WebParent/PS-Admin/src/main/java/com/petshop/admin/user/UserController.java
+++ PS-WebParent/PS-Admin/src/main/java/com/petshop/admin/user/UserController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -24,6 +26,11 @@ public class UserController {
 		return "pages/user/users";
 	}
 	
+	@GetMapping("/users/test")
+	public String viewPage() {
+		return "pages/user/test";
+	}
+	
 	@GetMapping("/users/new")
 	public String newUser(Model model) {
 		List<Role> listRoles = userService.listRoles();
@@ -32,6 +39,7 @@ public class UserController {
 		user.setEnabled(true);
 		model.addAttribute("user", user);
 		model.addAttribute("listRoles", listRoles);
+		model.addAttribute("pageTitle", "Create New User");
 		return "pages/user/user_form";
 	}
 	
@@ -39,6 +47,48 @@ public class UserController {
 	public String saveUser(User user, RedirectAttributes redirectAttributes) {
 		userService.save(user);
 		redirectAttributes.addFlashAttribute("message","The user has been saved successfully");
+		return "redirect:/users";
+	}
+	
+	@GetMapping("/users/edit/{id}")
+	public String editUser(@PathVariable(name = "id") Integer id,
+			RedirectAttributes redirectAttributes,
+			Model model) {
+		try {
+			User user = userService.get(id);
+			List<Role> listRoles = userService.listRoles();
+			model.addAttribute("user", user);
+			model.addAttribute("pageTitle", "Edit User (ID: " + id + ")");
+			model.addAttribute("listRoles", listRoles);
+		} catch (UserNotFoundException ex) {
+			redirectAttributes.addFlashAttribute("message", ex.getMessage());
+		}
+		
+		return "pages/user/user_form";
+	}
+	
+	@GetMapping("/users/delete/{id}")
+	public String deleteUser(@PathVariable("id") Integer id,
+			RedirectAttributes redirectAttributes,
+			Model model) {
+		try {
+			userService.delete(id);
+			redirectAttributes.addFlashAttribute("message", "The user ID " + id + "has been deleted successfully");
+		} catch (UserNotFoundException ex) {
+			redirectAttributes.addFlashAttribute("message", ex.getMessage());
+		}
+		return "redirect:/users";
+	}
+	
+	@GetMapping("/users/{id}/enabled/{status}")
+	public String updateUserEnabledStatus(
+			@PathVariable("id") Integer id, 
+			@PathVariable("status") boolean enabled,
+			RedirectAttributes redirectAttributes) {
+		userService.updateUserEnabledStatus(id, enabled);
+		String status = enabled ? "enabled" : "disabled";
+		String message = "The user ID " + id + " has been " + status;
+		redirectAttributes.addFlashAttribute("message", message);
 		return "redirect:/users";
 	}
 }
--- PS-WebParent/PS-Admin/src/main/java/com/petshop/admin/user/UserService.java
+++ PS-WebParent/PS-Admin/src/main/java/com/petshop/admin/user/UserService.java
@@ -1,6 +1,7 @@
 package com.petshop.admin.user;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -9,7 +10,10 @@ import org.springframework.stereotype.Service;
 import com.petshop.common.entity.Role;
 import com.petshop.common.entity.User;
 
+import jakarta.transaction.Transactional;
+
 @Service
+@Transactional
 public class UserService {
 	@Autowired private UserRepository userRepo;
 	@Autowired private RoleRepository roleRepo;
@@ -24,7 +28,18 @@ public class UserService {
 	}
 	
 	public void save(User user) {
-		encodePassword(user);
+		boolean isUpdatingUser = (user.getId() != null);
+		
+		if(isUpdatingUser) {
+			User existingUser = userRepo.findById(user.getId()).get();
+			
+			if(user.getPassword().isEmpty()) {
+				user.setPassword(existingUser.getPassword());
+			}
+			
+		}else {
+			encodePassword(user);
+		}
 		userRepo.save(user);
 	}
 	
@@ -33,8 +48,43 @@ public class UserService {
 		user.setPassword(encoderPassword);
 	}
 	
-	public boolean isEmailUnique(String email) {
+	public boolean isEmailUnique(Integer id,String email) {
 		User userByEmail = userRepo.getUserByEmail(email);
-		return userByEmail == null ;
+		
+		if(userByEmail == null) return true;
+		
+		boolean isCreatingNew = (id == null);
+		
+		if(isCreatingNew) {
+			if(userByEmail != null) return false;
+		}else {
+			if(userByEmail.getId() != id) {
+				return false;
+			}
+		}
+		
+		return true ;
+	}
+	
+	public User get(Integer id) throws UserNotFoundException {
+		try {
+			return userRepo.findById(id).get();
+		} catch (NoSuchElementException ex) {
+			// TODO: handle exception
+			throw new UserNotFoundException("Could not find any user with ID " + id);
+		}
+		
+	}
+	
+	public void delete(Integer id) throws UserNotFoundException {
+		Long countByIdLong = userRepo.countById(id);
+		if(countByIdLong == null || countByIdLong == 0) {
+			throw new UserNotFoundException("Could not find any user with ID " + id);
+		}
+		userRepo.deleteById(id);
+	}
+	
+	public void updateUserEnabledStatus(Integer id, boolean enabled) {
+		userRepo.updateEnabledStatus(id, enabled);
 	}
 }

a5cccd28460b6e91d3dc173c1fb86509a5b74ee0
MrTee <vokhanhloc1994@gmail.com>
2023-04-22 12:53:30 +0700

    Code Pagination for Users List

--- PS-WebParent/PS-Admin/src/main/java/com/petshop/admin/user/UserRepository.java
+++ PS-WebParent/PS-Admin/src/main/java/com/petshop/admin/user/UserRepository.java
@@ -3,13 +3,13 @@ package com.petshop.admin.user;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import com.petshop.common.entity.User;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends PagingAndSortingRepository<User, Integer>, CrudRepository<User, Integer> {
 
 	@Query("SELECT u FROM User u WHERE u.email = :email")
 	public User getUserByEmail(@Param("email") String email);
@@ -19,4 +19,5 @@ public interface UserRepository extends CrudRepository<User, Integer> {
 	@Query("UPDATE User u SET u.enabled = ?2 WHERE u.id = ?1")
 	@Modifying
 	public void updateEnabledStatus(Integer id, boolean enabled);
+	
 }
--- PS-WebParent/PS-Admin/src/main/java/com/petshop/admin/user/UserService.java
+++ PS-WebParent/PS-Admin/src/main/java/com/petshop/admin/user/UserService.java
@@ -4,6 +4,10 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -15,12 +19,18 @@ import jakarta.transaction.Transactional;
 @Service
 @Transactional
 public class UserService {
+	public static final int USERS_PER_PAGE = 4;
 	@Autowired private UserRepository userRepo;
 	@Autowired private RoleRepository roleRepo;
 	@Autowired private PasswordEncoder passwordEncoder;
 	
 	public List<User> listAll(){
-		return (List<User>) userRepo.findAll();
+		return (List<User>) userRepo.findAll(Sort.by("firstName").ascending());
+	}
+	
+	public Page<User> listByPage(int pageNum){
+		Pageable pageable = PageRequest.of(pageNum - 1, USERS_PER_PAGE);
+		return userRepo.findAll(pageable);
 	}
 	
 	public List<Role> listRoles(){
